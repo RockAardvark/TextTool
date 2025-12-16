bd0ad973847421cf03fb746ff80cf8c71837803c
RockAardvark <eml2favre@gmail.com>
2025-12-16 20:41:35 +0100

    Update texttool.py

--- texttool.py
+++ texttool.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-
+operations = {
+    "reverse": lambda text: text[::-1],
+    "upper": lambda text: text.upper(),
+    "count-words": lambda text: len(text.split()),
 
 def process_line(line):
     if " " not in line:
